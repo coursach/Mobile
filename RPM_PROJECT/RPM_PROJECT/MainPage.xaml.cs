5dbcd3d75324e877acbab7ada4035fd1c28a636e
KirillKhramkin <124364912+GoodGame563@users.noreply.github.com>
2024-04-21 19:16:16 +0300

    add files

---
+++ RPM_PROJECT/RPM_PROJECT/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RPM_PROJECT
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+                       
+        }
+    }
+}
---
+++ RPM_PROJECT/RPM_PROJECT/SubsPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RPM_PROJECT
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SubsPage : ContentPage
+	{
+		public SubsPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
