6ef2f6a4f4eb4b94aa4a1f1592c87219b8d094c4
nifle3 <kuper116rus.2005@gmail.com>
2024-05-16 18:08:53 +0300

    ксамарин иди нахуй

--- RPM_PROJECT/RPM_PROJECT.Android/Properties/AssemblyInfo.cs
+++ RPM_PROJECT/RPM_PROJECT.Android/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using Android.App;
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage, MaxSdkVersion = 32)]
+[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage, MaxSdkVersion = 32)]
