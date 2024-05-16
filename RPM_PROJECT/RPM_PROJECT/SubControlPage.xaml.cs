75221bf584e6a2a30e74ee5659033011afeb40a4
nifle3 <kuper116rus.2005@gmail.com>
2024-05-16 05:18:03 +0300

    qwe

--- RPM_PROJECT/RPM_PROJECT/SubControlPage.xaml.cs
+++ RPM_PROJECT/RPM_PROJECT/SubControlPage.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using RPM_PROJECT.api;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +9,40 @@ namespace RPM_PROJECT
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SubControlPage : ContentPage
 	{
-		public SubControlPage ()
+        protected override async void OnAppearing()
+        {
+			if (!Preferences.Get("haveSub", false))
+			{
+				button.IsVisible = false;
+				subscribeInfo.Text = "У вас нет подписки";
+				subscribeType.Text = "";
+				return;
+			}
+
+			subscribeType.Text = "Активная подписка";
+			subscribeInfo.Text = "";
+			button.IsVisible = true;
+
+			var result = await API.CheckUserSubscribe();
+
+			subscribeInfo.Text = $"{result.Name} {result.Title} {result.Price} {result.CountMonth}";
+
+            base.OnAppearing();
+        }
+
+        public SubControlPage ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+			if (!Preferences.Get("haveSub", false))
+				return; 
+
+			await API.DeleteSubsribe();
+			Preferences.Set("haveSub", false);
+            OnAppearing();
+		}
+    }
 }
